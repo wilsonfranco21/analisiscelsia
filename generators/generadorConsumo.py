ced0be7cf65bf1688441c88f44ba46ef9631424a
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-06-27 15:41:51 -0500

    se crea mock de consumo energetico

---
+++ generators/generadorConsumo.py
@@ -0,0 +1,16 @@
+import random
+def generarDatos():
+    datos = []
+    for i in range(5000):
+        dato={}
+        id=random.randint(1,10000)
+        referencia=random.choice(["ACH01","ACH22","ACH43"])
+        marca=random.choice(["kalley","sony","rico"])
+        capacidad=random.randint(100,2000)
+        ciudad=random.choice(["Medellin","Envigado","Sabaneta","La estrella","itagui"])
+        responsable=random.choice(["Jhon doe","Pedro Perez","Ana Rubio","Carlos Zapata","Martha builes"])
+        dato=[id,referencia,marca,capacidad,ciudad,responsable]
+        datos.append(dato)
+    return datos
+
+print(generarDatos())
