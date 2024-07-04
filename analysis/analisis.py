2f112da0354d940378c98ecae58f856438dac9f0
Christianramos <cramosari@cesde.net>
2024-07-04 17:31:47 -0500

    deleted

--- analysis/analisis.py
+++
@@ -1,8 +0,0 @@
-from generators.generadorConsumo import generarDatos
-import pandas as pd
-
-def analizarDatos():
-    datos=generarDatos()
-    tabla=pd.DataFrame(datos,columns=["id","referencia","marca","capacidad","consumo","ciudad","responsable"])
-    tabla.to_csv("./data/energiaAburraSur.csv",index=False)
-analizarDatos()
